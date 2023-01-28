d005456a4ed479130400c96bcf5ee4de255c3a35
Manuel Takata <takata211011@gmail.com>
2023-01-27 23:50:41 -0500

    change font size

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,9 +6,9 @@ import { BsThreeDots } from "react-icons/bs"
 function Card() {
   const like = true
   return (
-    <div className="flex flex-col shadow-2xl w-80 md:w-[300px] rounded-xl overflow-hidden ">
-      <Link href="/post/2">
-        <div className="relative">
+    <article className="flex flex-col shadow-2xl w-64 md:w-[300px] rounded-xl overflow-hidden ">
+      <Link href="/post/2" className="group">
+        <div className="relative ">
           <img
             className="aspect-[1.5]"
             src="https://images.pexels.com/photos/1055068/pexels-photo-1055068.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
@@ -16,9 +16,9 @@ function Card() {
             width="100%"
             height="200px"
           />
-          <div className="flex justify-between items-start  bg-black opacity-50 w-full h-full absolute top-0 p-5 text-white">
+          <div className="absolute top-0 flex items-start justify-between invisible w-full h-full p-5 text-white bg-black opacity-50 group-hover:visible">
             <div>
-              <h3 className="font-semibold text-xl">Fallin21</h3>
+              <h3 className="text-xl font-semibold">Fallin21</h3>
               <span className="font-medium">21 hours ago</span>
             </div>
             <button>
@@ -27,16 +27,16 @@ function Card() {
           </div>
         </div>
         <div className="px-4">
-          <div className="flex gap-2 my-3 flex-wrap">
-            <span className="text-gray-700">#croatia</span>
-            <span className="text-gray-700">#castle</span>
-            <span className="text-gray-700">#sunset</span>
-            <span className="text-gray-700">#beautiful</span>
+          <div className="flex flex-wrap gap-2 my-3">
+            <span className="text-xs text-gray-700">#croatia</span>
+            <span className="text-xs text-gray-700">#castle</span>
+            <span className="text-xs text-gray-700">#sunset</span>
+            <span className="text-xs text-gray-700">#beautiful</span>
           </div>
-          <h2 className="text-2xl font-semibold my-2">
+          <h2 className="text-xl font-semibold">
             Gray Concrete Triumphal Arch
           </h2>
-          <p className="text-gray-700">
+          <p className="text-sm font-medium text-gray-700">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus,
             natus tempora? Voluptates, exercitationem! Unde culpa nostrum atque
             repellendus animi
@@ -44,8 +44,8 @@ function Card() {
         </div>
       </Link>
 
-      <div className="px-4 py-2">
-        <div className="flex justify-between items-center gap-1 my-3  ">
+      <div className="px-4">
+        <div className="flex items-center justify-between gap-1 my-3 ">
           <button className="flex items-center gap-1 hover:text-gray-600">
             {like ? <FcLike /> : <FcLikePlaceholder />}
             <span>2 Like</span>
@@ -56,7 +56,7 @@ function Card() {
           </button>
         </div>
       </div>
-    </div>
+    </article>
   )
 }
 
