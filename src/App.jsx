a5570e5ca169587141b81ca74095b1de61b14c62
Manuel Takata <takata211011@gmail.com>
2023-01-27 23:49:44 -0500

    add auth routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react"
 import { Route, Switch } from "wouter"
 import Navbar from "./components/Navbar/Navbar"
+import Login from "./pages/Auth/Login"
+import Register from "./pages/Auth/Register"
 import Home from "./pages/Home/Home"
 import Post from "./pages/Post/Post"
 function App() {
@@ -10,6 +12,8 @@ function App() {
       <Switch>
         <Route path="/" component={Home} />
         <Route path="/post/:id" component={Post} />
+        <Route path="/auth/register" component={Register} />
+        <Route path="/auth/login" component={Login} />
       </Switch>
     </>
   )
