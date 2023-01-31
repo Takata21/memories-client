a870acf452c588bb1a87de46ef05798394234a98
Manuel Takata <takata211011@gmail.com>
2023-01-30 23:19:18 -0500

    remove navbar from auth pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
 import React from "react"
 import { Route, Switch } from "wouter"
 import Navbar from "./components/Navbar/Navbar"
-import Login from "./pages/Auth/Login"
+import SignIn from "./pages/Auth/SignIn"
 import Register from "./pages/Auth/Register"
 import Home from "./pages/Home/Home"
 import Post from "./pages/Post/Post"
+import useLocation from "wouter/use-location"
 function App() {
+  const [location] = useLocation()
+  console.log(location)
   return (
     <>
-      <Navbar />
+      {location !== "/auth/signin" ? (
+        location !== "/auth/register" ? (
+          <Navbar />
+        ) : null
+      ) : null}
+
       <Switch>
         <Route path="/" component={Home} />
         <Route path="/post/:id" component={Post} />
         <Route path="/auth/register" component={Register} />
-        <Route path="/auth/login" component={Login} />
+        <Route path="/auth/signin" component={SignIn} />
       </Switch>
     </>
   )
