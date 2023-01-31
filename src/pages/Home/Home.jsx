9f8772cfba051997961dffa0acf17867badb8138
Manuel Takata <takata211011@gmail.com>
2023-01-30 23:21:05 -0500

    fix styles from mobile devices

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,7 @@ import Search from "../../components/Search/Search"
 
 function Home() {
   return (
-    <div className="flex max-w-[1300px] min-h-screen py-4 mx-6 ">
+    <div className="flex-col-reverse items-center flex max-w-[1300px] min-h-screen py-4 mx-6 md:flex-row md:items-start ">
       <CardsContainer />
       <Search />
     </div>
